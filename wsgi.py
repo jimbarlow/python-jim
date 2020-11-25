fd95e5ff07244a9d818db0690b7cad140ac063d4
Jim Barlow <jim@jimbarlow.com>
2020-11-25 15:43:52 -0500

    added config.py for gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
 import flask
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/healthz")
+@application.route("/healthz")
 def healthz():
     resp = flask.Response("ok")
     resp.headers['Custom-Header'] = 'okbyjim'
     return resp
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
